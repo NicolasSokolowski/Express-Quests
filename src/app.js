89637cb0d658d105e503a252be36f1337b683b86
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-09-21 23:04:56 +0200

    Quest: express 03

--- src/app.js
+++ src/app.js
@@ -9,10 +9,12 @@ app.use(express.json());
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.put("/api/movies/:id", movieControllers.updateMovie);
 app.post("/api/movies", movieControllers.postMovie);
 
 app.get("/api/users", userController.getUsers);
 app.get("/api/users/:id", userController.getUserById);
+app.put("/api/users/:id", userController.updateUser);
 app.post("/api/users", userController.postUser);
 
 module.exports = app;
