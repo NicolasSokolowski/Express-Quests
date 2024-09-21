912db651cd19c4fd496d5d29e288be8c41f9f919
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-09-22 00:29:05 +0200

    Quest: express 03.2

--- src/app.js
+++ src/app.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const movieControllers = require("./controllers/movieControllers");
 const userController = require("./controllers/userController");
+const validateMovie = require("./middlewares/validateMovie");
+const validateUser = require("./middlewares/validateUser");
 
 const app = express();
 
 app.use(express.json());
 
-
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
-app.put("/api/movies/:id", movieControllers.updateMovie);
-app.post("/api/movies", movieControllers.postMovie);
+app.put("/api/movies/:id", validateMovie, movieControllers.updateMovie);
+app.post("/api/movies", validateMovie, movieControllers.postMovie);
 
 app.get("/api/users", userController.getUsers);
 app.get("/api/users/:id", userController.getUserById);
-app.put("/api/users/:id", userController.updateUser);
-app.post("/api/users", userController.postUser);
+app.put("/api/users/:id", validateUser, userController.updateUser);
+app.post("/api/users", validateUser, userController.postUser);
 
 module.exports = app;
