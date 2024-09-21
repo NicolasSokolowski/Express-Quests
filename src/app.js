1c3f0189e369ffe01b4249c9f27deb091538ad5e
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-09-21 20:51:28 +0200

    Quest: express 01.2

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const movieControllers = require("./controllers/movieControllers");
+const userController = require("./controllers/userController");
 
 const app = express();
 
-const movieControllers = require("./controllers/movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+
+app.get("/api/users", userController.getUsers);
+app.get("/api/users/:id", userController.getUserById);
 
 module.exports = app;
